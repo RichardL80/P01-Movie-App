ddd5a3eb85c79aba0bea5121e35576b79f630d2b
Richard Lewis <rlewis80@my.bcit.ca>
2025-04-21 21:34:26 -0700

    fix TMDB logo image

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -33,7 +33,7 @@ const PageAbout = () => {
             <div>This product uses the TMDB API but is not endorsed or certified by TMDB.</div>
             <div>
               {/* <img src="/public/images/TMDB-logo.svg" alt="TMDB logo" className='logo-small clickable'  */}
-              <img src="./images/TMDB-logo.svg" alt="TMDB logo" className='logo-small clickable' 
+              <img src="/P01-Movie-App/images/TMDB-logo.svg" alt="TMDB logo" className='logo-small clickable' 
                 onClick={() => window.open("https://developer.themoviedb.org/docs/getting-started", "_blank")} />
             </div>
           </div>
